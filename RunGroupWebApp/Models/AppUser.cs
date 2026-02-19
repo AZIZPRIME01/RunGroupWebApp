242b4ec9fd94d0028a6fe812ac6e99fc9c921067
Azizbek Axtamov <azizaxtamov0201@gmail.com>
2026-02-19 15:12:05 +0500

    Database is implemented and 2 controller are added::

---
+++ RunGroupWebApp/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using RunGroopWebApp.Models;
+namespace RunGroupWebApp.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int Pace { get; set; }
+        public int? Mileage { get; set; }
+        public Address? Adress { get; set; }
+    }
+}
